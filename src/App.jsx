c5458b61212b8e962153a39b81b7649beaa84b2d
Ayush765497 <ayushshrma765497@gmail.com>
2026-03-31 01:35:56 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -457,7 +457,7 @@ export default function App() {
       </main>
 
       <footer className="credit">
-        <span>Coded by <a href="https://www.instagram.com/berkindev/" target="_blank" rel="noopener noreferrer">berkindev</a></span>
+        <span>Coded by <a href="https://github.com/Ayush765497" target="_blank" rel="noopener noreferrer">berkindev</a></span>
       </footer>
     </div>
   );
